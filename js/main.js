49e302bb1406d49162e11e4483c610be39daacbb
Nihal Nayak <nihalnayak@gmail.com>
2016-02-06 07:22:44 +0530

    Final Commit before approval

--- js/main.js
+++ js/main.js
@@ -127,9 +127,18 @@ jQuery(function($) {'use strict';
 	      start();
 	    }
 	});
+                    
+    var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       false       // trigger animations on mobile devices (true is default)
+      }
+    );
 
 	//Initiat WOW JS
-	new WOW().init();
+	wow.init();
 	//smoothScroll
 	smoothScroll.init();
 
@@ -229,6 +238,8 @@ jQuery(function($) {'use strict';
 		});
 	}
 	google.maps.event.addDomListener(window, 'load', initialize_map);
+                    
+                    
 
 });
 
